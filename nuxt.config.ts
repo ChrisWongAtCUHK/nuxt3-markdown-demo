d20102d23b0381e9ea4b74545e4cb336b6cf7b89
Chris Wong <chris.wong.ksl@gmail.com>
2025-02-05 20:15:49 +0800

    GitHub Pages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,8 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@nuxtjs/mdc']
+  modules: ['@nuxtjs/mdc'],
+  app: {
+    baseURL: '/nuxt3-markdown-demo/'
+  }
 })
